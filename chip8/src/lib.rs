61ee68b7ee7d54e52aa3b46981e55836f92e329b
Christian Apostol <christiand.apostol@outlook.com>
2024-04-03 13:37:46 -0400

    more types to chip8 struct fields

--- chip8/src/lib.rs
+++ chip8/src/lib.rs
@@ -1,14 +1,20 @@
-// TODO: Create a struct that contains all of the CHIP8 Emulator's specifications
+mod stack;
+use stack::stack::Stack;
+
+const MEM_SIZE: usize = 4096;
+
+// TODO: Do we need stack pointer?
 struct chip8 {
-    memory: [u8; 4096], // 4 kb of RAM = 4096 bytes. The byte "memory address" will be the index in the array
+    memory: [u8; MEM_SIZE], // 4 kb of RAM = 4096 bytes. The byte "memory address" will be the index in the array
     registers: [u8; 16],
-    keyboard: [u8; 16], // 16 key hexadecimal keyboard
-    // display: bool, // TODO: should this be in the sdl project?
+    keyboard: [u8; 16], // 16 key hexadecimal keyboard 
     program_counter: u16, // points at instruction in memory
     index_register: u16, // 4kb = 4000 bytes which means 4000 memory addresses. 16 bits covers up to 65,536 different numbers
-    stack: bool,
+    stack: Stack<u16>,
     delay_timer: u8,
-    sound_timer: u8
+    sound_timer: u8,
+    // TODO: Framebuffer?
+    time: isize
 }
 
 
